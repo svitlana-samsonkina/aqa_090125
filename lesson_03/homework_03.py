937a4b8ca82c3db51ea5e01eb40f07b94cd191c7
Svitlana Samsonkina <s.samsonkyna@gmail.com>
2025-01-21 23:29:43 +0200

    completed homework 03

--- lesson_03/homework_03.py
+++ lesson_03/homework_03.py
@@ -1,4 +1,13 @@
-alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don't much care where ——" said Alice.\n"Then it doesn't matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you're sure to do that," said the Cat, "if you only walk long enough."'
+print("Task 01-03")
+alice_in_wonderland = (
+    '"Would you tell me, please, which way I ought to go from here?"\n'
+    '"That depends a good deal on where you want to get to," said the Cat.\n'
+    '"I don\'t much care where" —— said Alice.\n'
+    '"Then it doesn\'t matter which way you go," said the Cat.\n'
+    '—— "so long as I get somewhere," Alice added as an explanation.\n'
+    '"Oh, you\'re sure to do that," said the Cat, "if you only walk long enough."')
+print(alice_in_wonderland)
+
 # task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
 # task 02 == Знайдіть та екрануйте всі символи одинарної дужки у тексті
 # task 03 == Виведіть змінну alice_in_wonderland на друк
@@ -16,7 +25,12 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 моря становить 37 800 км2. Яку площу займають Чорне та Азов-
 ське моря разом?
 """
-
+print("Task 04")
+square_black_sea = 436402
+square_sea_of_azov = 37800
+square_total = int(square_black_sea + square_sea_of_azov)
+output = (f"Загальна площа Чорного та Азовського морів становить {square_total} км2.")
+print(output)
 
 # task 05
 """
@@ -25,7 +39,21 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 250 449 товарів. На другому та третьому – 222 950 товарів.
 Знайдіть кількість товарів, що розміщені на кожному складі.
 """
+print("Task 05")
+# Дано
+total_goods = 375291
+first_and_second = 250449
+second_and_third = 222950
 
+# Розв'язок
+second = first_and_second + second_and_third - total_goods
+first = first_and_second - second
+third = second_and_third - second
+
+# Результат
+print(f"Кількість товарів на першому складі: {first}")
+print(f"Кількість товарів на другому складі: {second}")
+print(f"Кількість товарів на третьому складі: {third}")
 
 # task 06
 """
@@ -34,7 +62,11 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 вати необхідно буде півтора року по 1179 грн/місяць. Обчисліть
 вартість комп’ютера.
 """
-
+print("Task 06")
+payment_per_month = 1179
+duration_of_payment = 1.5 * 12
+computer_cost = payment_per_month * duration_of_payment
+print(f"Вартість комп'ютера: {computer_cost} грн")
 
 # task 07
 """
@@ -43,7 +75,20 @@ a) 8019 : 8     d) 7248 : 6
 b) 9907 : 9     e) 7128 : 5
 c) 2789 : 5     f) 19224 : 9
 """
-
+print("Task 07")
+remainder_a = 8019%8
+remainder_b = 9907%9
+remainder_c = 2789%5
+remainder_d = 7248%6
+remainder_e = 7128%5
+remainder_f = 19224%9
+print(f"Остача від ділення 8019 на 8 дорівнює {remainder_a} \n"
+      f"Остача від ділення 9907 на 9 дорівнює {remainder_b} \n"
+      f"Остача від ділення 2789 на 5 дорівнює {remainder_c} \n"
+      f"Остача від ділення 7248 на 6 дорівнює {remainder_d} \n"
+      f"Остача від ділення 7128 на 5 дорівнює {remainder_e} \n"
+      f"Остача від ділення 19224 на 9 дорівнює {remainder_f} \n"
+      )
 
 # task 08
 """
@@ -57,7 +102,14 @@ c) 2789 : 5     f) 19224 : 9
 Торт            1           350 грн
 Вода            3           21 грн
 """
-
+print("Task 08")
+pizza_large = 4 * 274
+pizza_middle = 2 * 218
+juice = 4 * 35
+cake = 1 * 350
+water = 3 * 21
+total_cost = pizza_large + pizza_middle + juice + cake + water
+print(f"Загальна вартість замовлення: {total_cost} грн")
 
 # task 09
 """
@@ -66,7 +118,11 @@ c) 2789 : 5     f) 19224 : 9
 розміщено щонайбільше 8 фото. Скільки сторінок знадобиться
 Ігорю, щоб вклеїти всі фото?
 """
-
+print("Task 09")
+total_photos = 232
+photos_per_page = 8
+pages_needed = total_photos // photos_per_page
+print(f"Кількість сторінок, необхідна для вклеювання всіх фото: {pages_needed}")
 
 # task 10
 """
@@ -79,3 +135,13 @@ c) 2789 : 5     f) 19224 : 9
 равку під час цієї подорожі, кожного разу заправляючи пов-
 ний бак?
 """
+print("Task 10")
+distance_km = 1600
+fuel_per_100km = 9
+tank_capasity = 48
+
+total_fuel_needed = (distance_km / 100) * fuel_per_100km
+refuels_needed = total_fuel_needed // tank_capasity
+
+print(f"Необхідно бензину для подорожі: {total_fuel_needed} літрів")
+print(f"Кількість заправок, необхідна для подорожі: {int(refuels_needed)} рази")
