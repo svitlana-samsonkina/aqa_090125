165f8ce371805c1684d518101f3611ee389c3603
alex-pancho <pan.pan.4it@gmail.com>
2025-02-24 21:26:51 +0200

    lesson 11

--- lesson_11/lesson_11.py
+++ lesson_11/lesson_11.py
@@ -1,10 +1,82 @@
+""" Задано дані (список списків) про багаж (кількість речей і загальна вага багажу) пасажирів. 
+1. Скласти функцію, яка повертає тапл де міститься: 
+    * кількість пасажирів, які мають більше двох речей; 
+    * чи є хоч один пасажир, багаж якого складається з однієї речі вагою менше 25 кг; 
+    * число пасажирів, кількість речей яких перевершує середнє число речей всіх пасажирів.
+2. Напишіть тести для перевірки роботу функції на різних вхідних даних.
+# Алгоритм вирішення
+1. Проведіь розрахунок кількості пасажирів з більше ніж 2 речами
+2. Виконайте перевірку наявності пасажира з однією річчю вагою менше 25 кг
+3. Проведіь обчислення середнього числа речей
+4. З'ясуйте кількість пасажирів, які мають більше речей за середнє значення
+"""
 
-def second_in_line(long_long_text:str, word:str):
-    index = long_long_text.find(word)
-    assert index != -1, f"{word} not found in text"
-    index_2 = long_long_text.find(word, index + 1)
-    return index_2
+# Приклад вхідних даних
+passengers = [
+    {'number_of_items': 3, 'total_weight': 30},
+    {'number_of_items': 2, 'total_weight': 20},
+    {'number_of_items': 1, 'total_weight': 15},
+    {'number_of_items': 4, 'total_weight': 40},
+    {'number_of_items': 1, 'total_weight': 10},
+    {'number_of_items': 5, 'total_weight': 50},
+    {'number_of_items': 2, 'total_weight': 18},
+    {'number_of_items': 3, 'total_weight': 35},
+    {'number_of_items': 1, 'total_weight': 5},
+    {'number_of_items': 4, 'total_weight': 45}
+]
 
-long_txt = "hour after hour. And when the middle of the afternoon came, from being"
-word = "Tom"
-print(second_in_line(long_txt, word))
+import unittest
+
+def check_passanger(passengers): 
+    count_more_then_two_items = 0
+    has_one_item_less_25 = False
+    total_items = sum(p['number_of_items'] for p in passengers)
+    avg_items = total_items / len(passengers)
+    count_above_avg = 0 
+    
+    for p in passengers:
+        if p['number_of_items'] > avg_items:
+            count_above_avg += 1
+    
+    for p in passengers:
+        if p['number_of_items'] > 2:
+            count_more_then_two_items += 1
+            
+        if p['number_of_items'] == 1 and p['total_weight'] < 25:
+            has_one_item_less_25 = True
+
+    
+
+    return count_more_then_two_items, has_one_item_less_25, count_above_avg
+
+
+class TestCheckPassenger(unittest.TestCase):
+    
+    def test_count_more_then_two_items(self):
+        result = check_passanger(passengers)
+        self.assertEqual(result[0] , 5)
+        
+    def test_has_one_item_less_25(self):
+        result = check_passanger(passengers)
+        self.assertTrue(result[1])
+        
+    def test_calculate_avg_items(self):
+        result = check_passanger(passengers)
+        total_items = sum(p['number_of_items'] for p in passengers)
+        avg_items = total_items / len(passengers)
+        
+        count_above_avg = 0
+        for p in passengers:
+            if p['number_of_items'] > avg_items:
+                count_above_avg += 1
+        
+        self.assertEqual(result[2], count_above_avg)
+        
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+    
+    result = check_passanger(passengers)
+    print(result)
+        
+
+        
--- lesson_12/lesson_12.md
+++ lesson_12/lesson_12.md
@@ -1,18 +1,18 @@
-https://sharepad.io/live/VA8jhOl
-https://sharepad.io/live/33CrhkD
-https://sharepad.io/live/ChMaCg7
-https://sharepad.io/live/E4tJaKX
-https://sharepad.io/live/Ha79SzU
-https://sharepad.io/live/rbaRxOT
-https://sharepad.io/live/0rqyKYx
-https://sharepad.io/live/Xbnub3z
-https://sharepad.io/live/vEX760W
-https://sharepad.io/live/ib1pXJI
-https://sharepad.io/live/cdzmNnn
-https://sharepad.io/live/HBSdHr8
-https://sharepad.io/live/PkknhwR
-https://sharepad.io/live/roKBMmI
-https://sharepad.io/live/UcceH6V
+https://sharepad.io/live/VA8jhOl Антон          80
+https://sharepad.io/live/33CrhkD Катя           100
+https://sharepad.io/live/ChMaCg7 Денис          80
+https://sharepad.io/live/E4tJaKX Дмитро         100
+https://sharepad.io/live/Ha79SzU Константин Б   90
+https://sharepad.io/live/rbaRxOT Анна           80
+https://sharepad.io/live/0rqyKYx Леонід         95
+https://sharepad.io/live/Xbnub3z Наталія        0
+https://sharepad.io/live/vEX760W Світлана       100
+https://sharepad.io/live/ib1pXJI Вікторія       100
+https://sharepad.io/live/cdzmNnn Ксенія         100
+https://sharepad.io/live/HBSdHr8 Кирилл         80
+https://sharepad.io/live/PkknhwR Надія          100
+https://sharepad.io/live/roKBMmI Сергій         100
+https://sharepad.io/live/UcceH6V Володимир      100
 https://sharepad.io/live/lyyDIC5
 https://sharepad.io/live/Vf8vvU7
 https://sharepad.io/live/jC992Az
