7b8d8620c7869ddcbf6fadd73d0e1bf0f36e8654
alex-pancho <pan.pan.4it@gmail.com>
2025-03-20 20:41:15 +0200

    pep8 fix

--- lesson_17/lesson_17.py
+++ lesson_17/lesson_17.py
@@ -8,11 +8,12 @@ sys.path.append(str(project_folder))
 # sys.path.append(str(project_folder / "lesson_15"))
 # sys.path.append(str(project_folder / "lesson_16"))
 from lesson_15 import lesson_15 as l15
+
 # import lesson_16
 print(sys.path)
 
-car = l15.Car("lala","tratata")
-print(dir(car)) 
+car = l15.Car("lala", "tratata")
+print(dir(car))
 print(car.__dir__())
 print(dir())
 print(dir(__builtins__))
@@ -25,8 +26,9 @@ print(dir(__builtins__))
 # v = json.loads(json.dumps('123'))
 # print(v)
 
+
 # from json import (
-#   loads, 
+#   loads,
 #   'dict',
 #   'dir',
 #   'divmod',
